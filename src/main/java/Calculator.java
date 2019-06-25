37d35b8a24fe581fef4dbd08eec0ba87862956af
Piero Anesin <pieroanesin@gmail.com>
2019-06-25 22:30:02 +0200

    method add of Calculator refactored

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,39 +2,15 @@ public class Calculator {
 
     public int add(String numbers) {
 
-        int sum = 0;
-        int delimiterPosition;
-
         if (numbers.isEmpty()) {
             return 0;
         }
 
-        while (numbers.contains(",") || numbers.contains("\n")) {
-
-            delimiterPosition = 0;
-
-            if (numbers.contains(",") && numbers.contains("\n")) {
-                if (numbers.indexOf(",") < numbers.indexOf("\n")) {
-                    delimiterPosition = numbers.indexOf(",");
-                }
-                else {
-                    delimiterPosition = numbers.indexOf("\n");
-                }
-            }
-            else {
-                if (numbers.contains(",")) {
-                    delimiterPosition = numbers.indexOf(",");
-                }
-                if (numbers.contains("\n")) {
-                    delimiterPosition = numbers.indexOf("\n");
-                }
-            }
-
-            sum += Integer.parseInt(numbers.substring(0, delimiterPosition));
-            numbers = numbers.substring(delimiterPosition + 1);
+        int sum = 0;
+        String[] separateNumbers = numbers.split(",|\n");
+        for (String number:separateNumbers) {
+            sum += Integer.parseInt(number);
         }
-
-        sum += Integer.parseInt(numbers);
 
         return sum;
     }
