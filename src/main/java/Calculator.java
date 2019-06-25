b5089d70ea2cfa1cf95bf87105002d3766f98265
Piero Anesin <pieroanesin@gmail.com>
2019-06-25 16:26:54 +0200

    method add of Calculator refactored

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,17 +2,16 @@ public class Calculator {
 
     public int add(String numbers) {
 
-        int argumentOne;
-        int argumentTwo;
-
         if (numbers.isEmpty()) {
             return 0;
         }
 
         if (numbers.contains(",")) {
-            argumentOne = Integer.parseInt(numbers.substring(0,numbers.indexOf(",")));
-            argumentTwo = Integer.parseInt(numbers.substring(numbers.indexOf(",")+1));
-            return argumentOne + argumentTwo;
+            int sum = 0;
+            int comaPosition = numbers.indexOf(",");
+            sum += Integer.parseInt(numbers.substring(0,comaPosition));
+            sum += Integer.parseInt(numbers.substring(comaPosition+1));
+            return sum;
         }
 
         return Integer.parseInt(numbers);
