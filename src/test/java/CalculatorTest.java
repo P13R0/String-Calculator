b0e4b9e907951f474975573b058a211096882253
Piero Anesin <pieroanesin@gmail.com>
2019-06-25 12:18:47 +0200

    test unit refactored

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,19 +1,25 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
+    Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
     @Test
     public void sumForAnEmptyStringReturnZero() {
-        Calculator calculator = new Calculator();
 
         assertEquals(0, calculator.add(""));
     }
 
     @Test
     public void sumForASingleNumberReturnTheSameNumber() {
-        Calculator calculator = new Calculator();
 
         assertEquals(1, calculator.add("1"));
     }
