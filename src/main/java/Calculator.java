4cfebeb00da0c249390bed45d1e3b4444c011e51
Piero Anesin <pieroanesin@gmail.com>
2019-06-26 00:49:06 +0200

    methods of Calculator refactored

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,51 @@
 public class Calculator {
 
-    public int add(String numbers) {
+    public int add(String parameters) {
 
-        if (numbers.isEmpty()) {
+        if (parameters.isEmpty()) {
             return 0;
         }
 
+        String delimiters = getDelimiters(parameters);
+        String numbers = getNumbers(parameters);
+
+        return getNumbersSum(numbers, delimiters);
+    }
+
+
+    private String getDelimiters(String text) {
+
         String delimiters = ",|\n";
-        if (numbers.length() > 1 && numbers.substring(0,2).equals("//")) {
-            delimiters = numbers.substring(2,numbers.indexOf("\n"));
+
+        if (hasChangedStandardDelimiter(text)) {
+            delimiters = text.substring(2,text.indexOf("\n"));
+        }
+
+        return delimiters;
+    }
+
+
+    private boolean hasChangedStandardDelimiter(String text) {
+        return text.length() > 1 && text.substring(0,2).equals("//");
+    }
+
+
+    private String getNumbers(String text) {
+
+        String numbers = text;
+
+        if (hasChangedStandardDelimiter(numbers)) {
             numbers = numbers.substring(numbers.indexOf("\n")+1);
         }
+
+        return numbers;
+    }
+
+
+    private int getNumbersSum(String numbers, String delimiters) {
+
         int sum = 0;
+
         String[] separateNumbers = numbers.split(delimiters);
         for (String number:separateNumbers) {
             sum += Integer.parseInt(number);
